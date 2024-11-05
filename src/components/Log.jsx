70a71088aef2d54786164960c2c5b9abaec70252
Ivan Bosnic <ivan.bosnic@rimac-technology.com>
2024-11-05 08:42:51 +0100

    Added working game

---
+++ src/components/Log.jsx
@@ -0,0 +1,11 @@
+export default function Log({ turns }) {
+    return (
+        <ul id="log">
+            {turns.map((turn) => (
+                <li key={'${turn.square.row}${turn.square.col}'}>
+                    {turn.player} selected {turn.square.row}, {turn.square.col}
+                </li>
+            ))}
+        </ul>
+    );
+}
